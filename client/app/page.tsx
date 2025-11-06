9eb4d75bb3530c088b33724a86a37e8fd524296d
thelouisgram <hadesanoye01@gmail.com>
2025-11-05 16:57:12 -0800

    Homepage layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/Home/Banner";
 import CallToAction from "@/components/Home/CallToAction";
 import Features from "@/components/Home/Features";
+import Footer from "@/components/Home/Footer";
 import Hero from "@/components/Home/Hero";
 import Testimonials from "@/components/Home/Testimonials";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Features />
       <Testimonials />
       <CallToAction />
+      <Footer />
     </main>
   );
 }
--- client/types/home.ts
+++ client/types/home.ts
@@ -12,3 +12,20 @@ interface CardData {
 export interface CreateCardProps {
   card: CardData;
 }
+
+export interface FooterLink {
+  label: string;
+  href: string;
+}
+
+export interface SocialLink {
+  name: string;
+  href: string;
+  icon: React.ReactNode;
+}
+
+export interface FooterLinks {
+  product: FooterLink[];
+  resources: FooterLink[];
+  legal: FooterLink[];
+}
