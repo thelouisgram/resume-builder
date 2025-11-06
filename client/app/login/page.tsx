ed8a5e9bfee059acd135f6fc4dd4df26d67195d1
thelouisgram <hadesanoye01@gmail.com>
2025-11-06 09:18:07 -0800

    feat: implemented the login page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,13 +1,19 @@
 "use client";
-import { User2Icon } from "lucide-react";
+import { User2Icon, Mail, Lock } from "lucide-react";
 import React from "react";
+import { useSearchParams } from "next/navigation";
 
 interface FormInputEvent extends React.ChangeEvent<HTMLInputElement> {
   target: HTMLInputElement;
 }
 
 const Page = () => {
-  const [state, setState] = React.useState("login");
+    const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
+  const urlState = query === "register" ? "register" : "login";
+
+  const [state, setState] = React.useState(urlState || "login");
 
   const [formData, setFormData] = React.useState({
     name: "",
@@ -25,105 +31,79 @@ const Page = () => {
   };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
-    <form
-      onSubmit={handleSubmit}
-      className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white"
-    >
-      <h1 className="text-gray-900 text-3xl mt-10 font-medium">
-        {state === "login" ? "Login" : "Sign up"}
-      </h1>
-      <p className="text-gray-500 text-sm mt-2">Please {state} in to continue</p>
-      {state !== "login" && (
-        <div className="flex items-center mt-6 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-          <User2Icon  size={16} color="6b7280"/>
+      <form
+        onSubmit={handleSubmit}
+        className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white"
+      >
+        <h1 className="text-gray-900 text-3xl mt-10 font-medium">
+          {state === "login" ? "Login" : "Sign up"}
+        </h1>
+        <p className="text-gray-500 text-sm mt-2">
+          Please {state} in to continue
+        </p>
+        {state !== "login" && (
+          <div className="flex items-center mt-6 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <User2Icon size={16} color="#6B7280" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="border-none outline-none ring-0"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        )}
+        <div className="flex items-center w-full mt-4 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+          <Mail size={13} color="#6B7280" />
           <input
-            type="text"
-            name="name"
-            placeholder="Name"
+            type="email"
+            name="email"
+            placeholder="Email id"
             className="border-none outline-none ring-0"
-            value={formData.name}
+            value={formData.email}
             onChange={handleChange}
             required
           />
         </div>
-      )}
-      <div className="flex items-center w-full mt-4 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="14"
-          height="14"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#6B7280"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-mail-icon lucide-mail"
+        <div className="flex items-center mt-4 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+          <Lock size={13} color="#6B7280" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="border-none outline-none ring-0"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mt-4 text-left text-green-500">
+          <button className="text-sm" type="reset">
+            Forget password?
+          </button>
+        </div>
+        <button
+          type="submit"
+          className="mt-2 w-full h-11 rounded-full text-white bg-green-500 hover:opacity-90 transition-opacity"
         >
-          <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-          <rect x="2" y="4" width="20" height="16" rx="2" />
-        </svg>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email id"
-          className="border-none outline-none ring-0"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="flex items-center mt-4 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="14"
-          height="14"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#6B7280"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-lock-icon lucide-lock"
-        >
-          <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
-          <path d="M7 11V7a5 5 0 0 1 10 0v4" />
-        </svg>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          className="border-none outline-none ring-0"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="mt-4 text-left text-indigo-500">
-        <button className="text-sm" type="reset">
-          Forget password?
+          {state === "login" ? "Login" : "Sign up"}
         </button>
-      </div>
-      <button
-        type="submit"
-        className="mt-2 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity"
-      >
-        {state === "login" ? "Login" : "Sign up"}
-      </button>
-      <p
-        onClick={() =>
-          setState((prev) => (prev === "login" ? "register" : "login"))
-        }
-        className="text-gray-500 text-sm mt-3 mb-11"
-      >
-        {state === "login"
-          ? "Don't have an account?"
-          : "Already have an account?"}{" "}
-        <a href="#" className="text-indigo-500 hover:underline">
-          click here
-        </a>
-      </p>
-    </form>
+        <p
+          onClick={() =>
+            setState((prev) => (prev === "login" ? "register" : "login"))
+          }
+          className="text-gray-500 text-sm mt-3 mb-11"
+        >
+          {state === "login"
+            ? "Don't have an account?"
+            : "Already have an account?"}{" "}
+          <a href="#" className="text-green-500 hover:underline">
+            click here
+          </a>
+        </p>
+      </form>
     </div>
   );
 };
